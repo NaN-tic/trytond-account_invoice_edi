5d8cd1bf11e7e7ef981b411b17727f9f860e0b0b
Bernat Brunet <bernat@nan-tic.com>
2021-11-18 15:39:19 +0100

    Add the possibility to choose send or not the EDI file automatically.
    Add an advice when send the EDI file automatically.

---
+++ configuration.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from trytond.model import fields, ModelSQL, ModelView, ModelSingleton
+
+
+class InvoiceEdiConfiguration(ModelSingleton, ModelSQL, ModelView):
+    'Invoice Edi Configuration'
+    __name__ = 'invoice.edi.configuration'
+
+    edi_files_path = fields.Char('EDI Invoice Inbox Path')
+    outbox_path_edi = fields.Char('EDI Invoice Outbox Path')
+    separator = fields.Char('Separator')
+    automatic_edi_invoice_out = fields.Boolean('Send EDI file automatically')
+
+    @classmethod
+    def default_separator(cls):
+        return '|'
+
+    @classmethod
+    def default_automatic_edi_invoice_out(cls):
+        return True
